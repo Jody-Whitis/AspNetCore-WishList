6b9db923a2cdefc61a9ab3d39ad0ca4be814823b
Jody <jwhitis@JODYW-007041291>
2020-04-14 15:44:42 -0500

    adding property to dbcontent

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WishList.Model;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +12,10 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items
+        {
+            get;set;
+        }
         public ApplicationDbContext(DbContextOptions options)
           : base(options)
         {
